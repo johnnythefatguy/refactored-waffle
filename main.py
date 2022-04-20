1a0ca7dd883ccb1dd6fb3daef4dbb0bdbdc72803
Johnny Scott <johnnythefatguy@gmail.com>
2022-04-19 20:32:45 -0400

    commit on 'main'

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from neuralintents import GenericAssistant
 from textblob import TextBlob
-import requests, json, os, speech_recognition, pyttsx3, sys
+import requests, json, os, speech_recognition, pyttsx3, sys, subprocess
 from pygments import highlight, lexers, formatters
+
+code-name = "ash"
+name = code-name
 
 recognizer = speech_recognition.Recognizer()
 
 speaker = pyttsx3.init()
-speaker.setProperty('rate', 100)
+speaker.setProperty('rate', 150)
+
+def get_audio():
+    
+    with speech_recognition.Microphone(device_index=14) as mic:
+        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+        audio = recognizer.listen(mic)
+        messages = recognizer.recognize_google(audio)
+        messages = messages.lower()
+    return messages
 
 def greetings():
     lines = []
@@ -16,7 +28,7 @@ def greetings():
         for line in lines:
             count += 1
         if count == 0:
-            speaker.say("Hello, my name is Paul. How may I assist you?")
+            speaker.say(f"Hello, my name is {name}. How may I assist you?")
             speaker.runAndWait()
         elif count >= 1 and count <= 5:
             speaker.say("Hello Johnny, hope you're doing well today?")
@@ -33,15 +45,47 @@ def goodbyes():
     speaker.runAndWait()
     sys.exit(0)
 
-#def music():
-#    playlist = str(input("Please choose a playlist: "))
-#    Instance = vlc.Instance()
-#    player = Instance.media_player_new()
-#    Media = Instance.media_new(playlist.lower())
-#    Media.get_mrl()
-#    player.set_media(Media)
-#    player.play()
+def your_name():
+    speaker.say(f"My name is {name}")
+    speaker.runAndWait()
+
+def music():
+    global recognizer
     
+    done = False
+    
+    while done != True:
+        try:
+            speaker.say("Which playlist would you like to listen to?")
+            speaker.runAndWait()
+            with speech_recognition.Microphone(device_index=14) as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+                messages = recognizer.recognize_google(audio)
+                messages = messages.lower().replace(" ", "")
+            playlist = f"/home/Paul/Music/{messages}"
+            media_player = vlc.MediaPlayer()
+            media = vlc.Media(playlist)
+            media_player.set_media(media)
+            media_player.play()
+            time.sleep(5)
+
+            a = False
+
+            while a != True:
+                b = str(input("please type pause, play, or stop: "))
+                if b.lower() == "pause":
+                    media_player.pause()
+                elif b.lower() == "stop":
+                    media_player.stop()
+                    a = True
+                elif b.lower() == "play":
+                    media_player.play()
+                else:
+                    print("Please choose play, pause, or stop")
+        except speech_recognition.UnknownValueError():
+            recognizer = speech_recognition.Recognizer()
+            
 def weather():
     global recognizer
 
@@ -82,8 +126,8 @@ def todo_add():
         try:
             speaker.say("What would you like add to the list?")
             speaker.runAndWait()
-            with speech_recognition.Microphone as mic:
-                recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+            with speech_recognition.Microphone(device_index=14) as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
                 audio = recognizer.listen(mic)
                 messages = recognizer.recognize_google(audio)
                 messages = messages.lower()
@@ -104,8 +148,8 @@ def todo_remove():
         try:
             speaker.say("What is a keyword unique to the activity you wish to remove?")
             speaker.runAndWait()
-            with speech_recognition.Microphone as mic:
-                recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+            with speech_recognition.Microphone(device_index=14) as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
                 audio = recognizer.listen(mic)
                 messages = recognizer.recognize_google(audio)
                 messages = messages.lower()
@@ -147,8 +191,8 @@ def spellcheck():
         try:
             speaker.say("What word do you need help with?")
             speaker.runAndWait()
-            with speech_recognition.Microphone as mic:
-                recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+            with speech_recognition.Microphone(device_index=14) as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
                 audio = recognizer.listen(mic)
                 messages = recognizer.recognize_google(audio)
                 messages = messages.lower()         
@@ -160,25 +204,31 @@ def spellcheck():
 def feelings():
     speaker.say("As of right now this is what my health looks like.")
     speaker.runAndWait()
-    os.system("stacer")
+    subprocess.Popen("stacer")
     
-# future mappings , 'music' : music
         
-mappings = {'greeting' : greetings, 'weather' : weather, 'goodbye' : goodbyes, 'date' : dates, 'todo_add' : todo_add, 'todo_remove' : todo_remove, 'todo' : show_todo, 'spellcheck' : spellcheck, 'feeling' : feelings}
+mappings = {'greeting' : greetings, 'weather' : weather, 'goodbye' : goodbyes, 'date' : dates, 'todo_add' : todo_add, 'todo_remove' : todo_remove, 'todo' : show_todo, 'spellcheck' : spellcheck, 'feeling' : feelings, 'music' : music, "name": your_name}
 
 assistant = GenericAssistant('intents.json', intent_methods=mappings ,model_name="test_model")
 assistant.train_model()
 assistant.save_model()
 #assistant.load_model()
 
+WAKE = f"Hey {name}"
+
 while True:
-    try:
-        greetings()
-        with speech_recognition.Microphone as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.5)
-            audio = recognizer.listen(mic)
-            messages = recognizer.recognize_google(audio)
-            messages = messages.lower()
-        assistant.request(messages)
-    except speech_recognition.UnknownValueError:
-        recognizer = speech_recognition.Recognizer()
+    text = get_audio()
+    if text.count(WAKE) > 0:
+        try:
+            with speech_recognition.Microphone(device_index=14) as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+                messages = recognizer.recognize_google(audio)
+                messages = messages.lower()
+            assistant.request(messages)
+        except speech_recognition.UnknownValueError:
+            recognizer = speech_recognition.Recognizer()
+    else:
+        speaker.say("There is an error in my code")
+        speaker.runAndWait()
+        sys.exit(0)
